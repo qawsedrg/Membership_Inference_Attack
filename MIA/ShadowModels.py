bfcf2f8baa8cd521a9dbc7162d8a93411f874ea4
qawsedrg <42860644+qawsedrg@users.noreply.github.com>
2021-12-01 17:46:43 +0100

    qawsedrg

--- MIA/ShadowModels.py
+++ MIA/ShadowModels.py
@@ -1,15 +1,27 @@
 import torch
 from sklearn.model_selection import train_test_split
-import utils
+import torch.nn as nn
+from utils import *
 
 
 class ShadowModels:
-    def __init__(self, models, N, X, Y):
+    def __init__(self, models, N: int, X, Y, epoches, device):
         self.models = models
         self.N = N
         self.X = X
         self.Y = Y
-        self.shadowmodels = [self.train(model) for model in models]
+        self.epoches = epoches
+        self.device = device
+        self.shadowmodels = []
 
-    def train(self, model):
-        pass
+    def train(self):
+        for i in range(self.N):
+            model = self.models
+            optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+            shadow_X_train, shadow_X_test, shadow_Y_train, shadow_Y_test = train_test_split(self.X, self.Y,
+                                                                                            test_size=0.5,
+                                                                                            random_state=i)
+            loader = trainset(shadow_X_train, shadow_Y_train)
+            model = train(model, loader, self.device, optimizer=optimizer, criterion=nn.CrossEntropyLoss(),
+                          epoches=self.epoches)
+            self.shadowmodels.append(model)
--- MIA/utils.py
+++ MIA/utils.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+import torch.optim as optim
 
 
 class trainset(Dataset):
@@ -13,3 +14,24 @@ class trainset(Dataset):
 
     def __len__(self):
         return self.X.shape[0]
+
+
+def train(model, loader, device, optimizer, criterion, epoches):
+    for epoch in range(epoches):
+
+        running_loss = 0.0
+        for i, data in enumerate(loader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    return model
