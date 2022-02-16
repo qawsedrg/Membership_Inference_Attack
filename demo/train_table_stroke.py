1ac534442bcfab67b934bb4400bf6b40af2d35d6
吴逸帆 <yifan.wu@polytechnique.edu>
2022-02-16 16:29:17 +0100

    stroke

---
+++ demo/train_table_stroke.py
@@ -0,0 +1,112 @@
+# confidence boudary
+import pickle
+import os.path
+import argparse
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from tqdm import tqdm
+from MIA.utils import trainset, mix
+from model import Model1
+from opacus.optimizers import DPOptimizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", default=50, type=int)
+parser.add_argument("--batch_size", default=64, type=int)
+parser.add_argument("--save_to", default='models', type=str)
+parser.add_argument("--name", default='purchase', type=str, choices=["purchase", "location", "adult"], )
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data = pd.read_csv('../data/stroke.csv', nrows=600)
+    data = data[['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type',
+                 'avg_glucose_level', 'bmi', 'smoking_status', 'stroke']].dropna()
+    le = LabelEncoder()
+    for i in data.columns:
+        data[i] = le.fit_transform(data[i])
+    print(data)
+
+    X = np.array(data[['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type',
+                       'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status']]).astype(np.float32)
+    Y = np.array(data['stroke'])
+
+    target_X, shadow_X, target_Y, shadow_Y = train_test_split(X, Y, test_size=0.5, random_state=42)
+    target_train_X, target_test_X, target_train_Y, target_test_Y = train_test_split(target_X, target_Y, test_size=0.5,
+                                                                                    random_state=42)
+    shadow_train_X, shadow_test_X, shadow_train_Y, shadow_test_Y = train_test_split(shadow_X, shadow_Y, test_size=0.5,
+                                                                                    random_state=42)
+
+    trainloader = DataLoader(trainset(target_train_X, target_train_Y), batch_size=args.batch_size,
+                             shuffle=True)
+    testloader = DataLoader(trainset(target_test_X, target_test_Y), batch_size=args.batch_size,
+                            shuffle=False)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = Model1(10)
+    net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    dp_optimzer = DPOptimizer(
+        optimizer=optimizer,
+        noise_multiplier=1.0,
+        max_grad_norm=1.0,
+        expected_batch_size=4,
+    )
+
+    if not os.path.exists(args.save_to):
+        os.makedirs(args.save_to)
+
+    val_acc_max = 0
+    for epoch in range(args.n_epochs):
+        net.train()
+        epoch_loss = 0
+        acc = 0
+        with tqdm(enumerate(trainloader, 0), total=len(trainloader)) as t:
+            for i, data in t:
+                correct_items = 0
+
+                inputs, labels = data[0].to(device).long(), data[1].to(device).long()
+
+                optimizer.zero_grad()
+
+                outputs = net(inputs.float())
+                correct_items += torch.sum(torch.argmax(outputs, dim=-1) == labels).item()
+                acc_batch = correct_items / args.batch_size
+                acc += acc_batch
+
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item()
+                t.set_description("Epoch {:}/{:} Train".format(epoch + 1, 50))
+                t.set_postfix(accuracy="{:.3f}".format(acc / (i + 1)), loss="{:.3f}".format(epoch_loss / (i + 1)))
+
+        net.eval()
+        val_acc = 0
+        with torch.no_grad():
+            with tqdm(enumerate(testloader, 0), total=len(testloader)) as t:
+                for i, data in t:
+                    correct_items = 0
+
+                    inputs, labels = data[0].to(device), data[1].to(device)
+
+                    outputs = net(inputs)
+                    correct_items += torch.sum(torch.argmax(outputs, dim=-1) == labels).item()
+                    val_acc_batch = correct_items / args.batch_size
+                    val_acc += val_acc_batch
+
+                    t.set_description("Epoch {:}/{:} VAL".format(epoch + 1, args.n_epochs))
+                    t.set_postfix(accuracy="{:.3f}".format(val_acc / (i + 1)))
+        if val_acc > val_acc_max:
+            val_acc_max = val_acc
+            torch.save(net.state_dict(), os.path.join(args.save_to, args.name + ".pth"))
